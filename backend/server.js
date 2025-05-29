78763f2245c6a4fd665a88a9639f92b65868fec6
venkatsivanagu <venkatsivanagu@gmail.com>
2025-05-29 17:09:17 +0530

    change5

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,10 @@ app.use('/api/entries', entriesRouter);
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../office_bills/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../office_bills/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
   });
 }
 
